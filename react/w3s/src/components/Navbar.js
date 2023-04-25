14c39089328e5b7942d93688a78ec72f74cab11c
Kevin Huang <kevwhuang@gmail.com>
2023-04-25 00:48:36 -0500

    Feat: react-w3s navbar

--- react/w3s/src/components/Navbar.js
+++ react/w3s/src/components/Navbar.js
@@ -1,6 +1,21 @@
+import logo from '../assets/react.ico';
+
 function Navbar() {
+    const sites = ['Render', 'Netlify', 'Heroku'];
+    const links = sites.map((site, i) => {
+        return (
+            <li key={i}>
+                <a href={`https://www.${site}.com`} target="_blank">
+                    {site}
+                </a>
+            </li>
+        );
+    });
+
     return (
         <nav>
+            <ul className="menu">{links}</ul>
+            <img src={logo}></img>
         </nav>
     );
 }
