5ca3ccd3bad8bccc1d5f7da497216e752486f3e0
Kevin Huang <kevwhuang@gmail.com>
2023-05-27 01:35:18 -0500

    Added PWA configs

--- react/vite/vite.config.ts
+++ react/vite/vite.config.ts
@@ -1,8 +1,49 @@
 import autoprefixer from 'autoprefixer';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+const assets: string[] = [
+    'apple-touch-icon.png',
+    'favicon.ico',
+    'pwa-512x512.png',
+];
+
+const manifest: Object = {
+    background_color: '#000000',
+    description: '',
+    dir: 'ltr',
+    display: 'standalone',
+    lang: 'en',
+    name: 'vite',
+    orientation: 'any',
+    scope: '/',
+    short_name: 'vite',
+    start_url: '/',
+    theme_color: '#000000',
+    categories: [],
+    icons: [
+        {
+            purpose: 'any',
+            sizes: '192x192',
+            src: '/icons/pwa-192x192.png',
+            type: 'image/png',
+        }, {
+            purpose: 'any',
+            sizes: '512x512',
+            src: '/icons/pwa-512x512.png',
+            type: 'image/png',
+        }, {
+            purpose: 'any maskable',
+            sizes: '512x512',
+            src: '/icons/pwa-512x512.png',
+            type: 'image/png',
+        },
+    ],
+    screenshots: [],
+};
+
+const vite: any = defineConfig({
     appType: 'spa',
     base: '/',
     envDir: '.',
@@ -12,6 +53,11 @@ export default defineConfig({
     root: process.cwd(),
     plugins: [
         react(),
+        VitePWA({
+            manifest,
+            registerType: 'autoUpdate',
+            includeAssets: assets,
+        }),
     ],
     build: {
         outDir: 'dist',
@@ -30,3 +76,5 @@ export default defineConfig({
         port: 3000,
     },
 });
+
+export default vite;
